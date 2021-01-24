9880b3287fe40c065c1f2765961ed234a68dbc39
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2021-01-25 01:02:56 +0900

    add-checkbox-menubox

--- src/App.js
+++ src/App.js
@@ -11,9 +11,44 @@ const App = () => {
           <h1 className="title">Japanese-typhoon</h1>
         </div>
       </section>
-      <ChoroplethMapPage />
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-one-quarter">
+              {/*プルダウンメニュー 画面3分の1*/}
+              <div className="box" style={{ height: "100%" }}>
+                <b>
+                  <font size="5">Filter memu</font>
+                </b>
+                <div className="field">
+                  <label className="label">台風何号の説明とか？</label>
+                  <div className="control">
+                    <p>23tyhg</p>
+                  </div>
+                </div>
+
+                <div className="field">
+                  <label className="label">
+                    ここらへんで点の説明とかするかなー？
+                  </label>
+                </div>
+
+                <div className="field">
+                  <label className="label">。。。。</label>
+                  <div className="control">
+                    {/* <div className="select is-fullwidth"></div> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <ChoroplethMapPage />
+            </div>
+          </div>
+        </section>
+      </div>
     </body>
-    // </Router>
   );
 };
 
